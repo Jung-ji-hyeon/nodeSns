f95f045407f22be64b995b8591eaa55483387fb2
Jung-ji-hyeon <ejxldortm@naver.com>
2024-09-12 21:53:51 +0900

    게시글, 해시태그 검색을 통해 가져갈 수 있게 해주는 API 라우터 개발 및 토큰 재발급 개발

--- call/controllers/index.js
+++ call/controllers/index.js
@@ -1,27 +1,45 @@
 const axios = require('axios');
 
-exports.test = async (req, res, next) => { // 토큰 테스트 라우터
+const URL = process.env.API_URL;
+axios.defaults.headers.origin = process.env.ORIGIN;
+
+const request = async (req, api) => {
   try {
-    if (!req.session.jwt) { // 세션에 토큰이 없으면 토큰 발급 시도
-      const tokenResult = await axios.post('http://localhost:8002/v1/token', {
+    if (!req.session.jwt) {
+      const tokenResult = await axios.post(`${URL}/token`, {
         clientSecret: process.env.CLIENT_SECRET,
       });
-      if (tokenResult.data?.code === 200) { // 토큰 발급 성공
-        req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
-      } else { // 토큰 발급 실패
-        return res.json(tokenResult.data); // 발급 실패 사유 응답
-      }
+      req.session.jwt = tokenResult.data.token;
     }
-    // 발급받은 토큰 테스트
-    const result = await axios.get('http://localhost:8002/v1/test', {
+    return await axios.get(`${URL}${api}`, {
       headers: { authorization: req.session.jwt },
-    });
-    return res.json(result.data);
+    })
+  } catch (error) {
+    if (error.response?.status === 419) {
+      delete req.session.jwt;
+      return request(req, api);
+    }
+    throw error.response;
+  }
+};
+
+exports.getMyPosts = async (req ,res, next) => {
+  try {
+  const result = await request(req, '/posts/my');
+  res.json(result.data);
   } catch (error) {
     console.error(error);
-    if (error.response?.status === 419) { // 토큰 만료 시
-      return res.json(error.response.data);
-    }
-    return next(error);
+    next(error);
+  }
+
+};
+
+exports.searchByHashtag = async (req, res, next) => {
+  try {
+  const result = await request(req, `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`);
+  res.json(result.data);
+  } catch (error) {
+    console.error(error);
+    next(error);
   }
 };
