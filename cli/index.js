5f2e75443aff0af828bc5cf4f21b6d86b692c0c3
Jung-ji-hyeon <ejxldortm@naver.com>
2024-10-02 21:11:25 +0900

    CLI 프로그램 개발

---
+++ cli/index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const readLine = require('readline');
+
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.clear();
+const answerCallback = (answer) => {
+    if (answer === 'y') {
+        console.log('감사합니다.');
+        rl.close();
+    } else if (answer === 'n') {
+        console.log('죄송합니다.');
+        
+    } else {
+        console.clear();
+        console.log('y 또는 n만 입력하세요.');
+        rl.question('예제가 재미있습니까? (y/n)', answerCallback);
+    }
+};
+
+rl.question('예제가 재미있습니까? (y/n)', answerCallback);
